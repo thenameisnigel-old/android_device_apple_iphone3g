9e5f00d2c4d327e3264cf6f4bde5a82d1b776525
Ryan Norris <rnorris97@bertie.k12.nc.us>
2013-06-21 19:40:01 -0300

    Update iPhone3G.mk

--- iPhone3G.mk
+++ iPhone3G.mk
@@ -1,21 +1,37 @@
-LOCAL_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
-COMMON := $(subst iPhone3G,common,$(LOCAL_DIR))
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := device/apple/iphone3g
 
 ifeq ($(TARGET_BUILD_TYPE),debug)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/sdk.mk)
 else
-$(call inherit-product, $(COMMON)/iPhone.mk)
-endif
-
-$(call inherit-product, $(COMMON)/common.mk)
+$(call inherit-product, device/apple/iphone-common/iPhone.mk)
+endif.mk)
 
 PRODUCT_NAME := iPhone3G
 PRODUCT_DEVICE := iPhone3G
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/apple/overlay/mbxlite
 
+
+# Keylayout
 PRODUCT_COPY_FILES += \
 	$(LOCAL_DIR)gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
 	$(LOCAL_DIR)asound.conf:system/etc/asound.conf \
 	$(LOCAL_DIR)asound.state:system/etc/asound.state
+	
 
+
+$(call inherit-product, device/apple/iphone-common/common
